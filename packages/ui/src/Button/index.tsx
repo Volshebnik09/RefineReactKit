b549357ac564be43ff97ec95be9c3643e728e901
sin <shulgin.grigoriy@yandex.ru>
2025-01-16 11:27:02 +0300

    темизация

--- packages/ui/src/Button/index.tsx
+++ packages/ui/src/Button/index.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
 const Button = styled.button<ButtonProps>(props=>({
     color: 'red',
     backgroundColor: props.primary ? 'yellow' : 'blue',
-    fontSize: props.theme.fontSize?.main || '18px'
+    fontSize: props.theme.fontSize?.md || '18px'
 }))
 
 export {
