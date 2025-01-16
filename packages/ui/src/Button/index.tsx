2bc56876c746b5213cb42f89125f46eed1db161f
sin <shulgin.grigoriy@yandex.ru>
2025-01-16 10:56:51 +0300

    Победа над SSR

--- packages/ui/src/Button/index.tsx
+++ packages/ui/src/Button/index.tsx
@@ -1,10 +1,9 @@
+import styled from "@emotion/styled";
 import React from "react";
 
-const Button = (props: React.PropsWithChildren) => {
-    return <button className={`button`}>
-        {props.children}
-    </button>
-}
+const Button = styled.button`
+  color: turquoise;
+`
 
 export {
     Button
