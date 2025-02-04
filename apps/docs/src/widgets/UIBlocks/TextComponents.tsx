92be3cc0a420b37328bb8ea871190ff1c339b5c6
sin <shulgin.grigoriy@yandex.ru>
2025-02-05 00:22:17 +0300

    save

--- apps/docs/src/widgets/UIBlocks/TextComponents.tsx
+++ apps/docs/src/widgets/UIBlocks/TextComponents.tsx
@@ -1,12 +1,11 @@
 "use client";
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Panel, Text} from "@refine-react-kit/ui";
-import reactElementToJSXString from 'react-element-to-jsx-string';
+
 
 function TextComponents() {
-    const codeBlockRef = React.useRef<HTMLDivElement>(null);
-    const content = (
-        <>
+    return (
+        <Panel>
             <Text.H1>H1 Text component</Text.H1>
             <Text.H2>H2 Text component</Text.H2>
             <Text.H3>H3 Text component</Text.H3>
@@ -14,21 +13,6 @@ function TextComponents() {
             <Text.H5>H5 Text component</Text.H5>
             <Text.H6>H6 Text component</Text.H6>
             <Text.Span>Span Text component</Text.Span>
-            <Text.P>Paragraph Text component</Text.P>
-        </>
-    )
-    useEffect(()=>{
-        const codeString = reactElementToJSXString(content)
-        if (!codeBlockRef.current) return
-        codeBlockRef.current.textContent = codeString
-    }, [])
-
-    return (
-        <Panel>
-            {content}
-            <pre>
-                <code ref={codeBlockRef}/>
-            </pre>
         </Panel>
     );
 }
